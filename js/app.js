79b11b27a537c3bb4aeddb5045455b5d3f3d9eac
Kristian <kristian.lindin@me.com>
2014-08-09 09:22:57 +0200

    structure pre angular pageTag animate

--- js/app.js
+++ js/app.js
@@ -6,4 +6,27 @@ angular.module('joansApp', ['ngRoute'])
 			// controller: 'mainCtrl',
 			templateUrl: 'templates/home.html'
 		})
+		.when('/historien', {
+			// controller: 'mainCtrl',
+			templateUrl: 'templates/historien.html'
+		})
+		.when('/media', {
+			// controller: 'mainCtrl',
+			templateUrl: 'templates/media.html'
+		})
+		.when('/latarna', {
+			// controller: 'mainCtrl',
+			templateUrl: 'templates/latarna.html'
+		})
+		.when('/medverkande', {
+			// controller: 'mainCtrl',
+			templateUrl: 'templates/medverkande.html'
+		})
+		.when('/kontakt', {
+			// controller: 'mainCtrl',
+			templateUrl: 'templates/kontakt.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
 });
